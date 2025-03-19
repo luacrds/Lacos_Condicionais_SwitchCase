027660b4239973de5161f7afa7f0117b7f8f1ee1
unknown <luanacardosos@outlook.com>
2025-03-19 15:12:47 -0300

    second commit

--- Lacos_Condicionais/src/Lista_1/Atividade_1.java
+++ Lacos_Condicionais/src/Lista_1/Atividade_1.java
@@ -1,5 +1,33 @@
 package Lista_1;
 
+import java.util.Scanner;
+
 public class Atividade_1 {
+	
+	public static void main(String[] args) {
+		
+		Scanner leia = new Scanner(System.in);
+		
+		int a, b,c;
+		
+		System.out.print("Digite o primeiro valor ");
+        a= leia.nextInt();
+        
+        System.out.print("Digite o segundo valor ");
+        b= leia.nextInt();
+        
+        System.out.print("Digite o terceiro valor ");
+        c= leia.nextInt();
+        
+        int somando_valores = a+b;
+        
+        if (somando_valores > c) {
+        	System.out.print("A soma de a+b é maior que c");
+        }
+        
+        if (somando_valores < c) {
+        	System.out.print("A soma de a+b é menor que c");
+        }
+	}
 
 }
